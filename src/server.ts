dacf7a876e7856ec915c2f6ea841b7b92a30521a
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2024-08-20 18:28:02 -0300

    Instalando o prisma

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from "express";
 import 'express-async-errors';
+import cors from 'cors';
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
@@ -19,7 +21,7 @@ app.use((err:Error, req: Request, res:Response, next:NextFunction) => {
         status: 'error',
         message:'Internal server error.'
     })
-    
+
 });
 
 
