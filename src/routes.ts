5ede13845ceb3ac0719f2d65413d53b0dbea1622
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2024-08-21 10:48:16 -0300

    criando estrutura de controles e serviços

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { Router, Request, Response } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
-router.get('/teste', (req:Request, res: Response) =>{
-    return res.json({ok: true})
-})
+//router.get('/teste', (req:Request, res: Response) =>{
+//    return res.json({ok: true})
+//})
+
+// --- Rotas User ---
+
+router.post('/users', new CreateUserController().handle)
+
 
 export { router };
