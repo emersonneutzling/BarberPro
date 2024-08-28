2ebafba6d73da8f50755d21dfa48dd4f87697fe9
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2024-08-28 08:17:21 -0300

    Criando middleware para autenticação

---
+++ src/controllers/user/DetailUserController.ts
@@ -0,0 +1,19 @@
+import { Request, Response} from "express";
+import { DetailUserService } from "../../services/user/DetailUserService";
+
+class DetailUserController{
+    async handle(request: Request, response: Response){
+
+        const user_id = request.user_id;
+
+        console.log(user_id);
+
+        const detailUserService = new DetailUserService();
+
+        const detailUser = await detailUserService.execute();
+
+        return response.json(detailUser);
+    } 
+}
+
+export { DetailUserController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import { isAuthenticted } from "./middlewares/isAuthenticted";
 
 const router = Router();
 
@@ -9,6 +12,7 @@ const router = Router();
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticted, new DetailUserController().handle)
 
 
 export { router };
